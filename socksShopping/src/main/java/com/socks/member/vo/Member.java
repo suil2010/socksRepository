ea166f862f657627e8a054e4dbfcb087cea2e6de
Eunjung <ejjang0813@naver.com>
2017-10-30 17:16:42 +0900

    vo 만들었어요

--- socksShopping/src/main/java/com/socks/member/vo/Member.java
+++ socksShopping/src/main/java/com/socks/member/vo/Member.java
@@ -1,5 +1,117 @@
 package com.socks.member.vo;
 
-public class Member {
+import java.io.Serializable;
 
+public class Member implements Serializable{
+	private String memberId;
+	private String name;
+	private String password;
+	private String address;
+	private String email;
+	private String point;
+	public Member() {
+	}
+	public Member(String memberId, String name, String password, String address, String email, String point) {
+		this.memberId = memberId;
+		this.name = name;
+		this.password = password;
+		this.address = address;
+		this.email = email;
+		this.point = point;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPoint() {
+		return point;
+	}
+	public void setPoint(String point) {
+		this.point = point;
+	}
+	@Override
+	public String toString() {
+		return "Member [memberId=" + memberId + ", name=" + name + ", password=" + password + ", address=" + address
+				+ ", email=" + email + ", point=" + point + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((memberId == null) ? 0 : memberId.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((point == null) ? 0 : point.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Member other = (Member) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (memberId == null) {
+			if (other.memberId != null)
+				return false;
+		} else if (!memberId.equals(other.memberId))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (point == null) {
+			if (other.point != null)
+				return false;
+		} else if (!point.equals(other.point))
+			return false;
+		return true;
+	}
+	
 }
