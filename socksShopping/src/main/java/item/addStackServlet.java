ca83d3cae127a4a893b7ffaaa652bb6f53ed823f
LeeSuIl <suil2010@naver.com>
2017-11-01 21:26:31 +0900

    itemform변경

--- socksShopping/src/main/java/item/addStackServlet.java
+++ socksShopping/src/main/java/item/addStackServlet.java
@@ -14,17 +14,6 @@ import javax.servlet.http.HttpServletResponse;
 public class addStackServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
